1d59dcf7240bda14661b5b7349729a11ca87a6ee
rayanne-on <rayanne.oliveira.bento@ccc.ufcg.edu.br>
2024-08-18 13:03:47 -0300

    exibe lição

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,28 +1,24 @@
 import Licao
 import Exercicio
-import Sprites (makeTextLines)
+import Sprites (formataLinhasTexto, licao1)
 
-ex1 :: Char -> Exercicio
-ex1 char = Exercicio [(char, "green"), (char, "red"), (char, "default"), (char, "default"),
-    (char, "default"), (char, "default"), (char, "default"), (char, "default")] "nao_iniciado"
+exibeLicao :: Licao -> [(Char, String)]
+exibeLicao licao = getExercio (exercicios licao)
 
-ex2 :: Char -> Char -> Exercicio
-ex2 char1 char2 = Exercicio [(char1, "green"), (char1, "red"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char1, "default"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char1, "default"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char1, "default"), (char1, "default"), (char2, "default")] "nao_iniciado"
+getExercio :: [Exercicio] -> [(Char, String)]
+getExercio [] = []
+getExercio (ex:exercicios) = if Exercicio.status ex == "nao_iniciado" 
+                                        then Exercicio.exercicio ex
+                                        else getExercio exercicios
 
-ex3 :: Char -> Char -> Exercicio
-ex3 char1 char2 = Exercicio [(char1, "green"), (char2, "red"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char2, "default"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char2, "default"), (char1, "default"), (char2, "default"),
-    (char1, "default"), (char2, "default"), (char1, "default"), (char2, "default")] "nao_iniciado"
-
-licao1 :: Licao
-licao1 = Licao "instrucao" [ex1 'j', ex2 'f' 'j', ex3 '_' 'j'] "nao_iniciado"
 
 main :: IO ()
 main = do
-    let textLines = makeTextLines (exercicio (ex3 '_' 'j'))  " "
+    -- let entrada = 'jjjjajjj'
+
+    -- let textLines = formataLinhasTexto (exercicio (ex1 'j'))  " "
+    -- mapM_ putStrLn textLines
+
+    let textLines = formataLinhasTexto (exibeLicao licao1) " "
 
     mapM_ putStrLn textLines
