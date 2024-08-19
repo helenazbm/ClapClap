c0b31c2247f0bf0662e27f394f54ece60aa1078f
rayanne-on <rayanne.oliveira.bento@ccc.ufcg.edu.br>
2024-08-19 11:26:37 -0300

    encapsula métodos

--- haskell/Licao.hs
+++ haskell/Licao.hs
@@ -7,3 +7,26 @@ data Licao = Licao {
     exercicios :: [Exercicio],
     status :: String
 } deriving (Show, Read)
+
+getExercio :: [Exercicio] -> Exercicio
+getExercio (ex:exercicios) = if Exercicio.status ex == "nao_iniciado" 
+                            then ex
+                            else getExercio exercicios
+
+getExercicioLicao :: [Licao] -> Exercicio
+getExercicioLicao (licao:licoes) = if Licao.status licao == "nao_iniciado" || Licao.status licao == "em_processo"
+                                then getExercio (exercicios licao)
+                                else getExercicioLicao licoes
+
+alteraCorExercicio :: String -> [(Char, String)] -> [(Char, String)]
+alteraCorExercicio entrada [] = []
+alteraCorExercicio (en:entrada) ((gabarito, cor):gabaritos) = if en == gabarito
+                                                            then [(gabarito, "green")] ++ corrigeExercicio entrada gabaritos
+                                                            else [(gabarito, "red")] ++ corrigeExercicio entrada gabaritos
+
+contaErros :: String -> [(Char, String)] -> Int
+contaErros (en:entrada) ((gabarito, cor):gabaritos) = 0
+
+corrigeExercicio :: String -> [(Char, String)] -> [(Char, String)]
+corrigeExercicio entrada exercicio = alteraCorExercicio entrada exercicio
+
--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,28 +1,9 @@
-import Licao
 import Exercicio
+import Licao (getExercio, getExercicioLicao, corrigeExercicio)
 import Sprites (formataLinhasTexto, licoes, getDadosLicoes, getDadosExercicios, setDadosExercicios)
 
-getExercio :: [Exercicio] -> Exercicio
-getExercio (ex:exercicios) = if Exercicio.status ex == "nao_iniciado" 
-                            then ex
-                            else getExercio exercicios
 
-getExercicioLicao :: [Licao] -> Exercicio
-getExercicioLicao (licao:licoes) = if Licao.status licao == "nao_iniciado" || Licao.status licao == "em_processo"
-                                then getExercio (exercicios licao)
-                                else getExercicioLicao licoes
-
-corrigeExercicio :: String -> [(Char, String)] -> [(Char, String)]
-corrigeExercicio entrada [] = []
-corrigeExercicio (en:entrada) ((gabarito, cor):gabaritos) = if en == gabarito
-                                                            then [(gabarito, "green")] ++ corrigeExercicio entrada gabaritos
-                                                            else [(gabarito, "red")] ++ corrigeExercicio entrada gabaritos
-
--- teste :: String -> Exercicio -> [(Char, String)]
--- teste entrada ex = 
---     let _ = setDadosExercicios "1" "1"
---     in corrigeExercicio entrada (exercicio ex)
-
+-- iniciarLicao
 main :: IO ()
 main = do
     dadosLicoes <- getDadosLicoes
@@ -37,5 +18,3 @@ main = do
     let textLines2 = formataLinhasTexto (corrigeExercicio entrada (exercicio ex)) " "
     setDadosExercicios (Exercicio.id ex) (Exercicio.idLicao ex)
     mapM_ putStrLn textLines2
-
-
