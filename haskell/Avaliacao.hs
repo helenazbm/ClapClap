814593ac9896d890671098034106fd12e256502b
marseathais <marcia.thais.revoredo.aires@ccc.ufcg.edu.br>
2024-08-21 22:37:28 -0300

    funções adicionais para avaliacao

--- haskell/Avaliacao.hs
+++ haskell/Avaliacao.hs
@@ -2,18 +2,14 @@ module Avaliacao where
 
 import Exercicio
 import Licao
+import Sprites (licao1)
 
 data Avaliacao = Avaliacao {
         percentualAcertos :: Int,
         palavrasPorMinuto :: Int
     } deriving (Show, Read)
 
-contaLetrasExercicios :: [(Char, String)] -> Int
-contaLetrasExercicios [] = 0
-contaLetrasExercicios ((gabarito, cor):gabaritos) = 1 + contaLetrasExercicios gabaritos
-
 contaErrosExercicios :: String -> [(Char, String)] -> Int
-contaErrosExercicios [] _ = 0
 contaErrosExercicios _ [] = 0
 contaErrosExercicios (en:entrada) ((gabarito, cor):gabaritos) =
     if en == gabarito
@@ -21,7 +17,28 @@ contaErrosExercicios (en:entrada) ((gabarito, cor):gabaritos) =
         else 1 + contaErrosExercicios entrada gabaritos
 
 calculaPrecisaoExercicios :: String -> [(Char, String)] -> Float
-calculaPrecisaoExercicios entrada gabaritos =
-    let contaLetras = contaLetrasExercicios gabaritos
-        contaErros = contaErrosExercicios entrada gabaritos
-    in fromIntegral (contaErros * 100) / fromIntegral contaLetras
+calculaPrecisaoExercicios entrada gabaritos =  
+    100 - fromIntegral (contaErrosExercicios entrada gabaritos * 100) / fromIntegral (length entrada)
+
+-- retorna a quantidade de estrelas do exercicio 
+calculaPontuacaoExercicio :: String -> [(Char, String)] -> Int
+calculaPontuacaoExercicio entrada gabarito =
+    let precisao = calculaPrecisaoExercicios entrada gabarito
+    in min 3 (ceiling (precisao * 3))
+
+
+avaliaLicao :: [(Char, String)] -> [String] -> Float
+avaliaLicao _ [] = 0
+avaliaLicao gabaritos exercicios =
+    let pontuacoes = map (\exercicio -> fromIntegral (calculaPontuacaoExercicio exercicio gabaritos)) exercicios
+        totalPontuacoes = sum pontuacoes
+        numExercicios = fromIntegral (length exercicios)
+    in if numExercicios == 0
+        then 0
+        else totalPontuacoes / numExercicios
+
+exibirAvaliacao :: [(Char, String)] -> [String] -> IO ()
+exibirAvaliacao gabaritos respostas = do
+    let pontuacao = avaliaLicao gabaritos respostas
+    putStrLn $ "Pontuação da lição: " ++ show pontuacao
+
