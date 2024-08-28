daeba234a387e30197ee160b9ba1b1b205ba5e6d
Lucas André <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2024-08-27 22:09:14 -0300

    adiciona tags de cor

--- haskell/Util.hs
+++ haskell/Util.hs
@@ -2,11 +2,11 @@ module Util where
 
 import System.IO
 import System.Console.ANSI
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
 import System.Process(callCommand)
 import Control.Concurrent (threadDelay)
 import System.IO.Unsafe (unsafePerformIO)
-import Data.List (intercalate)
-import Data.List.Split (splitOn)
 
 
 limparTela :: IO()
@@ -33,6 +33,11 @@ coloreTexto semCor =
 substituiTags :: String -> String
 substituiTags = replace "[AZUL]" "\ESC[34m"
             . replace "[DEFAULT]" "\ESC[0m"
+            . replace "[VERMELHO]" "\ESC[31m"
+            . replace "[VERDE]" "\ESC[32m"
+            . replace "[AMARELO]" "\ESC[33m"
+            . replace "[MAGENTA]" "\ESC[35m"
+            . replace "[CIANO]" "\ESC[36m"
 
 replace :: Eq a => [a] -> [a] -> [a] -> [a]
 replace old new = intercalate new . splitOn old
