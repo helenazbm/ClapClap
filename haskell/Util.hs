46266679644baf1bca562ae5a9633bb0ebca5251
Lucas André <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2024-08-28 00:11:49 -0300

    colore 10 lições

--- haskell/Util.hs
+++ haskell/Util.hs
@@ -10,7 +10,7 @@ import System.IO.Unsafe (unsafePerformIO)
 
 
 limparTela :: IO()
-limparTela = callCommand "clear"
+limparTela = callCommand "cls"
 
 delay :: IO ()
 delay = threadDelay (1 * 1000000) -- 1.0 segundo
