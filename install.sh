86e770fd69a874741f8a40cc4d4c7ac88a5f4cdb
MFernandaRicarte <maria.fernanda.fernandes.ricarte@ccc.ufcg.edu.br>
2024-08-29 23:15:08 -0300

    adiciona arquivo instalações bash

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Atualizando o sistema..."
+sudo apt update
+
+echo "Instalando o cabal-install..."
+sudo apt install -y cabal-install
+
+echo "Atualizando cabal..."
+cabal update
+
+echo "Instalando as bibliotecas necessárias..."
+cabal install split --lib
+cabal install directory --lib
+cabal install process --lib
+cabal install random --lib
+cabal install time --lib
+cabal install async --lib
+cabal install ansi-terminal --lib
+
+echo "Todas as dependências foram instaladas com sucesso! Agora está tudo pronto para você usar o ClapClap!"
