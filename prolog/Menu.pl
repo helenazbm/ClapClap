5bcc65f7608b8f73d189a8a948e08e3a3a27d40e
Maria Helena Barros <maria.helena.barros.moraes@ccc.ufcg.edu.br>
2024-09-21 12:08:13 -0300

    adiciona menus; desafios, tutorial

--- prolog/Menu.pl
+++ prolog/Menu.pl
@@ -9,8 +9,8 @@ imprime_menu :-
     opcoes_menu_principal(OpcaoMenu).
 
 opcoes_menu_principal('l') :- lista_Licoes.
-opcoes_menu_principal('d') :- writeln('desafios').
-opcoes_menu_principal('t') :- writeln('tutoriais').
+opcoes_menu_principal('d') :- lista_desafios.
+opcoes_menu_principal('t') :- exibe_tutorial.
 opcoes_menu_principal('s') :- sair.
 opcoes_menu_principal(_) :- imprime_menu.
 
@@ -23,6 +23,23 @@ lista_Licoes :-
         (   NumeroLicao >= 1, NumeroLicao =< 15 -> writeln(['Você escolheu a lição:', NumeroLicao])
         ; lista_Licoes)
     ).
+
+lista_desafios :-
+    limpar_tela,
+    ler_arquivo('../dados/arteTxt/desafios.txt'),
+    ler_entrada(Entrada),
+    ( Entrada = "" -> imprime_menu; Entrada = "r" -> writeln('Exibir o ranking'); number_string(NumeroDesafio, Entrada), opcoes_menu_desafios(NumeroDesafio)).
+
+opcoes_menu_desafios(1) :- writeln('Desafio 1').
+opcoes_menu_desafios(2) :- writeln('Desafio 2').
+opcoes_menu_desafios(3) :- writeln('Desafio 3').
+opcoes_menu_desafios(_) :- lista_desafios.
+
+exibe_tutorial:-
+    limpar_tela,
+    ler_arquivo('../dados/arteTxt/tutorial.txt'),
+    ler_entrada(_),
+    imprime_menu.
 
 sair :-
     limpar_tela,
