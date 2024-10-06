f3637c1f4ef035531209bd37292b8d107d359f2e
rayanne-on <rayanne.oliveira.bento@ccc.ufcg.edu.br>
2024-10-06 01:46:05 -0300

    corrige exercicio

--- prolog/Exercicio.pl
+++ prolog/Exercicio.pl
@@ -1,1 +1,10 @@
-:- module(Exercicio).
+:- module(Exercicio, [corrige_exercicio/3]).
+
+corrige_exercicio([], [], []).
+corrige_exercicio([], [[Gab, _]|Gabarito], [[Gab, "vermelho"]|R]) :-
+    corrige_exercicio([], Gabarito, R).
+corrige_exercicio([En|Entrada], [[Gab, _]|Gabarito], [[Gab, "verde"]|R]) :-
+    En = Gab,
+    corrige_exercicio(Entrada, Gabarito, R), !.
+corrige_exercicio([En|Entrada], [[Gab, _]|Gabarito], [[Gab, "vermelho"]|R]) :- 
+    corrige_exercicio(Entrada, Gabarito, R).
