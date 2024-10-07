155fe1d5ccca5da96dfdffdd66037af482ba434c
Lucas André <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2024-10-07 16:20:43 -0300

    Implementa lições coloridas

--- prolog/Utils.pl
+++ prolog/Utils.pl
@@ -1,4 +1,4 @@
-:- module(Utils, [limpar_tela/0, delay/0,ler_entrada/1, ler_arquivo/1, concatena_strings/2, unlines/2, lines/2, insere_espaços/2]).
+:- module(Utils, [limpar_tela/0, delay/0,ler_entrada/1, ler_arquivo/1, ler_instrucao/1, concatena_strings/2, unlines/2, lines/2, insere_espaços/2, replace_substring/4]).
 
 :- use_module('./Sprites.pl').
 
@@ -16,6 +16,20 @@ ler_arquivo(NomeArquivo) :-
     open(NomeArquivo, read, Fluxo),
     ler_linhas(Fluxo),
     close(Fluxo).
+
+    ler_instrucao(NomeArquivo) :-
+    open(NomeArquivo, read, Fluxo),
+    ler_linhas_instrucao(Fluxo),
+    close(Fluxo).
+
+ler_linhas_instrucao(Fluxo) :-
+    read_line_to_string(Fluxo, Linha),
+    (   Linha \= end_of_file
+    ->  aplica_cor_instrucao(Linha, LinhaColorida),
+        writeln(LinhaColorida),
+        ler_linhas_instrucao(Fluxo)
+    ;   true
+    ).
 
 ler_linhas(Fluxo) :-
     read_line_to_string(Fluxo, Linha),
@@ -40,3 +54,10 @@ insere_espaços(N, Espaços) :-
     N1 is N - 1,
     insere_espaços(N1, EspaçosAnteriores),
     string_concat(' ', EspaçosAnteriores, Espaços).
+
+replace_substring(String, To_Replace, Replace_With, Result) :-
+    sub_string(String, Before, _, After, To_Replace),
+    sub_string(String, 0, Before, _, Prefix),
+    sub_string(String, _, After, 0, Suffix),
+    string_concat(Prefix, Replace_With, Temp),
+    string_concat(Temp, Suffix, Result).
